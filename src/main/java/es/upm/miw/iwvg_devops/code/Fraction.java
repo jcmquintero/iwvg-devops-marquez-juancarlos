e4cc132111ebcf69299d90d1000ba3eae177ce63
Juan Carlos <marquez28@correo.ugr.es>
2022-10-05 17:05:50 +0200

    enhanced Fraction #10

--- src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
+++ src/main/java/es/upm/miw/iwvg_devops/code/Fraction.java
@@ -68,5 +68,56 @@ public class Fraction {
                 '}';
     }
 
+    public boolean isProper() {
+        return this.getNumerator() < this.getDenominator();
+    }
+
+    public boolean isImproper() {
+        return !this.isProper();
+    }
+
+    public boolean isEquivalent(Fraction fraction) {
+        return this.decimal() == fraction.decimal();
+    }
+
+    public Fraction add(Fraction fraction) {
+        int lNumerator;
+        int lDenominator;
+        Fraction result = null;
+
+        if (this.getDenominator() != 0 && fraction.getDenominator() != 0) {
+            result = new Fraction();
+            if (this.getDenominator() == fraction.getDenominator()) {
+                lNumerator = this.getNumerator() + fraction.getNumerator();
+                lDenominator = this.getDenominator();
+            } else {
+                lNumerator = this.getNumerator() * fraction.getDenominator() + fraction.getNumerator() * this.getDenominator();
+                lDenominator = this.getDenominator() * fraction.getDenominator();
+            }
+            result.setNumerator(lNumerator);
+            result.setDenominator(lDenominator);
+        }
+
+        return result;
+    }
+
+    public Fraction multiply(Fraction fraction) {
+        if (this.getDenominator() != 0 && fraction.getDenominator() != 0) {
+            return new Fraction(this.getNumerator() * fraction.getNumerator(),
+                    this.getDenominator() * fraction.getDenominator());
+        } else {
+            return null;
+        }
+    }
+
+    public Fraction divide(Fraction fraction) {
+        if (this.getDenominator() != 0 && fraction.getDenominator() != 0) {
+            return new Fraction(this.getNumerator() * fraction.getDenominator(),
+                    this.getDenominator() * fraction.getNumerator());
+        } else {
+            return new Fraction(0, 0);
+        }
+    }
+
 }
 
